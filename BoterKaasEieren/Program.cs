6fabf231be68337b0373ed5748dc237b5d665bf4
Peter-The-Great <PjotrW15@Gmail.com>
2023-08-30 16:10:16 +0200

    Add project files.

---
+++ BoterKaasEieren/Program.cs
@@ -0,0 +1,201 @@
+﻿using System;
+namespace BoterKaasEieren
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Spel spel = new Spel();
+            spel.Start();
+        }
+    }
+
+    class Spel
+    {
+        private Veld veld;
+        private Speler spelerX;
+        private Speler spelerO;
+        private Speler huidigeSpeler;
+
+        public Spel()
+        {
+            veld = new Veld();
+            spelerX = new Speler('X');
+            spelerO = new Speler('O');
+            huidigeSpeler = spelerX;
+        }
+
+        public void Start()
+        {
+            while (!veld.IsVol() && !veld.HeeftIemandGewonnen())
+            {
+                Console.Clear();
+                veld.Toon();
+                Console.WriteLine($"Huidige speler: {huidigeSpeler.GetSymbool()}");
+
+                Console.Write("Geef de x-coördinaat (1-3): ");
+                int x = int.Parse(Console.ReadLine()) - 1;
+
+                Console.Write("Geef de y-coördinaat (1-3): ");
+                int y = int.Parse(Console.ReadLine()) - 1;
+
+                Zet zet = new Zet(huidigeSpeler, new Coördinaat(x, y));
+                if (veld.IsZetGeldig(zet))
+                {
+                    veld.VoerZetUit(zet);
+                    huidigeSpeler = (huidigeSpeler == spelerX) ? spelerO : spelerX;
+                }
+                else
+                {
+                    Console.WriteLine("Ongeldige zet. Probeer opnieuw.");
+                    Console.ReadLine();
+                }
+            }
+
+            Console.Clear();
+            veld.Toon();
+            if (veld.HeeftIemandGewonnen())
+            {
+                Speler winnendeSpeler = (huidigeSpeler == spelerX) ? spelerO : spelerX;
+                Console.WriteLine($"Speler {winnendeSpeler.GetSymbool()} heeft gewonnen!");
+            }
+            else
+            {
+                Console.WriteLine("Gelijkspel!");
+            }
+        }
+    }
+
+    class Veld
+    {
+        private char[,] data;
+
+        public Veld()
+        {
+            data = new char[,] { { ' ', ' ', ' ' }, { ' ', ' ', ' ' }, { ' ', ' ', ' ' } };
+        }
+
+        public void Toon()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(data[i, j]);
+                    if (j < 2) Console.Write(" | ");
+                }
+                Console.WriteLine();
+                if (i < 2) Console.WriteLine("---------");
+            }
+        }
+
+        public bool IsZetGeldig(Zet zet)
+        {
+            int x = zet.Coördinaat.GetX();
+            int y = zet.Coördinaat.GetY();
+            return x >= 0 && x < 3 && y >= 0 && y < 3 && data[y, x] == ' ';
+        }
+
+        public void VoerZetUit(Zet zet)
+        {
+            int x = zet.Coördinaat.GetX();
+            int y = zet.Coördinaat.GetY();
+            data[y, x] = zet.Speler.GetSymbool();
+        }
+
+        public bool IsVol()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (data[i, j] == ' ')
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        public bool HeeftIemandGewonnen()
+        {
+            // Controleer horizontale en verticale lijnen
+            for (int i = 0; i < 3; i++)
+            {
+                if (data[i, 0] != ' ' && data[i, 0] == data[i, 1] && data[i, 1] == data[i, 2])
+                {
+                    return true;
+                }
+
+                if (data[0, i] != ' ' && data[0, i] == data[1, i] && data[1, i] == data[2, i])
+                {
+                    return true;
+                }
+            }
+
+            // Controleer diagonale lijnen
+            if (data[0, 0] != ' ' && data[0, 0] == data[1, 1] && data[1, 1] == data[2, 2])
+            {
+                return true;
+            }
+
+            if (data[0, 2] != ' ' && data[0, 2] == data[1, 1] && data[1, 1] == data[2, 0])
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+    }
+
+    class Speler
+    {
+        private char symbool;
+
+        public Speler(char symbool)
+        {
+            this.symbool = symbool;
+        }
+
+        public char GetSymbool()
+        {
+            return symbool;
+        }
+    }
+
+    class Zet
+    {
+        public Speler Speler { get; }
+        public Coördinaat Coördinaat { get; }
+
+        public Zet(Speler speler, Coördinaat coördinaat)
+        {
+            Speler = speler;
+            Coördinaat = coördinaat;
+        }
+    }
+
+    class Coördinaat
+    {
+        private int x;
+        private int y;
+
+        public Coördinaat(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public int GetX()
+        {
+            return x;
+        }
+
+        public int GetY()
+        {
+            return y;
+        }
+    }
+}
---
+++ BoterKaasEieren/Simple.cs
@@ -0,0 +1,97 @@
+﻿namespace Simple
+{
+    using System;
+
+    namespace TicTacToe
+    {
+        class Program
+        {
+            static char[] board = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+            static int playerTurn = 1; // 1 for Player 1, 2 for Player 2
+
+            static void Main(string[] args)
+            {
+                bool gameRunning = true;
+
+                while (gameRunning)
+                {
+                    DrawBoard();
+                    int choice = GetPlayerChoice();
+                    UpdateBoard(choice);
+                    if (CheckForWin())
+                    {
+                        DrawBoard();
+                        Console.WriteLine($"Player {playerTurn} wins!");
+                        gameRunning = false;
+                    }
+                    else if (IsBoardFull())
+                    {
+                        DrawBoard();
+                        Console.WriteLine("It's a draw!");
+                        gameRunning = false;
+                    }
+                    else
+                    {
+                        playerTurn = (playerTurn == 1) ? 2 : 1;
+                    }
+                }
+            }
+
+            static void DrawBoard()
+            {
+                Console.Clear();
+                Console.WriteLine($" {board[0]} | {board[1]} | {board[2]} ");
+                Console.WriteLine("---+---+---");
+                Console.WriteLine($" {board[3]} | {board[4]} | {board[5]} ");
+                Console.WriteLine("---+---+---");
+                Console.WriteLine($" {board[6]} | {board[7]} | {board[8]} ");
+            }
+
+            static int GetPlayerChoice()
+            {
+                int choice = -1;
+                while (choice < 1 || choice > 9 || board[choice - 1] == 'X' || board[choice - 1] == 'O')
+                {
+                    Console.Write($"Player {playerTurn}, choose a position (1-9): ");
+                    int.TryParse(Console.ReadLine(), out choice);
+                }
+                return choice;
+            }
+
+            static void UpdateBoard(int choice)
+            {
+                char symbol = (playerTurn == 1) ? 'X' : 'O';
+                board[choice - 1] = symbol;
+            }
+
+            static bool CheckForWin()
+            {
+                // Check for winning combinations: rows, columns, diagonals
+                if ((board[0] == board[1] && board[1] == board[2]) ||
+                    (board[3] == board[4] && board[4] == board[5]) ||
+                    (board[6] == board[7] && board[7] == board[8]) ||
+                    (board[0] == board[3] && board[3] == board[6]) ||
+                    (board[1] == board[4] && board[4] == board[7]) ||
+                    (board[2] == board[5] && board[5] == board[8]) ||
+                    (board[0] == board[4] && board[4] == board[8]) ||
+                    (board[2] == board[4] && board[4] == board[6]))
+                {
+                    return true;
+                }
+                return false;
+            }
+
+            static bool IsBoardFull()
+            {
+                foreach (char position in board)
+                {
+                    if (position != 'X' && position != 'O')
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        }
+    }
+}
